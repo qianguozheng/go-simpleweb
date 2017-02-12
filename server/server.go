b5f889df245512a1fc0291ccf331b0edd6885699
qianguozheng <guozhengqian0825@126.com>
2017-02-12 21:25:49 +0800

    Add simple auth page into trunk

--- server/server.go
+++ server/server.go
@@ -10,6 +10,14 @@ import (
 func DBInit(){
 	//sqlite.Connect()
 }
+
+func StaticServer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "text/html")
+	staticHandler := http.FileServer(http.Dir("./template/"))
+	staticHandler.ServeHTTP(w, r)
+	return
+}
+
 func Run()  {
 	handler := http.NewServeMux()
 	handler.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request){
@@ -19,10 +27,13 @@ func Run()  {
 	handler.Handle("/upgrade", UpgradeHandler())
 	handler.Handle("/control", ControlHandler())
 
+	handler.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	//handler.HandleFunc("/", index)
+	handler.HandleFunc("/index.html", index)
 	////Config
 	//handler.Handle("/config/upgrade", ConfigUpgHandler())
 	//handler.Handle("/config/control", ConfigCtlHandler())
 
-	http.ListenAndServe(":80", handler)
+	http.ListenAndServe(":38001", handler)
 
 }
